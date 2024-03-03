33b53376117f38fe6c97bc338cd60a6e21534a40
bernard kung <speak.freely@gmail.com>
2024-03-03 02:55:50 -0500

    arrows displaying

--- src/components/Combo.js
+++ src/components/Combo.js
@@ -1,12 +1,27 @@
+import up from '../assets/up.svg';
+import right from '../assets/right.svg';
+import down from '../assets/down.svg';
+import left from '../assets/left.svg';
+
 
 const Combo = ({ combo }) => {
-
+  console.log(combo.combo.split(','))
+  const arrows = {
+    "up"    : up,
+    "right" : right,
+    "down"  : down,
+    "left"  : left,
+  }
 
   return (
-    <div>
+    <div className={'flexCol comboContainer'}>
       <span>{ combo.name }</span>
       <span>{ combo.category }</span>
-      <span>{ combo.combo }</span>
+      <span className={'flexRow'}>
+        { combo.combo.split(',').map(c=>(
+          <img className={'arrow'} src={arrows[c]} />
+        ))}
+      </span>
     </div>
   )
 }
