7006c8e910faed37e3eac3cac8811b64ec78736b
bernard kung <speak.freely@gmail.com>
2024-03-03 02:39:52 -0500

    displaying combos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import Combo from './components/Combo';
 import combos from './data/helldivers2combos.json';
 
 function App() {
 
-
+  console.log(combos)
 
   return (
     <div className="App">
-        
-
+      <p>test</p>
+      {combos.map(combo => (
+        <Combo combo={combo} />
+      ))}
     </div>
   );
 }
---
+++ src/components/Combo.js
@@ -0,0 +1,14 @@
+
+const Combo = ({ combo }) => {
+
+
+  return (
+    <div>
+      <span>{ combo.name }</span>
+      <span>{ combo.category }</span>
+      <span>{ combo.combo }</span>
+    </div>
+  )
+}
+
+export default Combo
